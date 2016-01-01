fde985ef93a79e7672cb7d881c72b91737ca3e19
jhalaac <jhalaac@INjhalaac.local>
2016-01-01 15:12:00 +0530

    draws a cross or a circle based on the user

--- javascript.js
+++ javascript.js
@@ -1,16 +1,22 @@
+      var user = 1
+      function canvasClicked(element) {
+        var cellToBeFilled = element.getContext("2d");
+        if(user == 1) {
+            cellToBeFilled.beginPath();
+            cellToBeFilled.moveTo(10,10);
+            cellToBeFilled.lineTo(40,40);
+            cellToBeFilled.moveTo(40,10);
+            cellToBeFilled.lineTo(10,40);
+            cellToBeFilled.stroke();
+            cellToBeFilled.closePath();
+            user =2;
+          }
+          else {
+            cellToBeFilled.beginPath();
+            cellToBeFilled.arc(25,25,20,0,Math.PI*2,true);
+            cellToBeFilled.stroke();
+            cellToBeFilled.closePath();
+            user =1;
+          }
 
-      function canvasClicked(numb) {
-        var c = document.getElementById(numb);
-        var cxt = c.getContext("2d");
-        /*cxt.beginPath();
-            cxt.moveTo(10,10);
-            cxt.lineTo(40,40);
-            cxt.moveTo(40,10);
-            cxt.lineTo(10,40);
-            cxt.stroke();
-            cxt.closePath();*/
-            cxt.beginPath();
-            cxt.arc(25,25,20,0,Math.PI*2,true);
-            cxt.stroke();
-            cxt.closePath();
       }
