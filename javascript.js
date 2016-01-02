90f9ba7e97c4160908d43b11bc286832e37cad9d
jhalaac <jhalaac@INjhalaac.local>
2016-01-02 17:18:19 +0530

    gave appropriate messages when each player one

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
       var user = 1
       var numberOfClicks = 0;
       var painted=new Array();
-      
+      var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
       window.onload =function(){
-        for(var i =1;i<=9;i++){
+        for(var i =0;i<9;i++){
           painted[i]=false;
         }
       };
@@ -13,28 +13,39 @@
         idOfElement=idOfElement.replace("canvas","")
 
         if(painted[idOfElement]== false) {
-        painted[idOfElement]=true;
-        var cellToBeFilled = element.getContext("2d");
-        if(user == 1) {
-            cellToBeFilled.beginPath();
-            cellToBeFilled.moveTo(10,10);
-            cellToBeFilled.lineTo(40,40);
-            cellToBeFilled.moveTo(40,10);
-            cellToBeFilled.lineTo(10,40);
-            cellToBeFilled.stroke();
-            cellToBeFilled.closePath();
-            user =2;
-          }
-          else {
-            cellToBeFilled.beginPath();
-            cellToBeFilled.arc(25,25,20,0,Math.PI*2,true);
-            cellToBeFilled.stroke();
-            cellToBeFilled.closePath();
-            user =1;
-          }
-          numberOfClicks++;
-          if(numberOfClicks==9) {
-            alert("GAME OVER");
-          }
+
+          var cellToBeFilled = element.getContext("2d");
+          if(user == 1) {
+              cellToBeFilled.beginPath();
+              cellToBeFilled.moveTo(10,10);
+              cellToBeFilled.lineTo(40,40);
+              cellToBeFilled.moveTo(40,10);
+              cellToBeFilled.lineTo(10,40);
+              cellToBeFilled.stroke();
+              cellToBeFilled.closePath();
+              user =2;
+              painted[idOfElement]='x';
+            }
+            else {
+              cellToBeFilled.beginPath();
+              cellToBeFilled.arc(25,25,20,0,Math.PI*2,true);
+              cellToBeFilled.stroke();
+              cellToBeFilled.closePath();
+              user =1;
+              painted[idOfElement]='o';
+            }
+            numberOfClicks++;
+            checkForWinners(painted[idOfElement]);
+            if(numberOfClicks==9) {
+              alert("GAME OVER");
+            }
         }
+      }
+      function checkForWinners(symbol){
+        console.log(symbol);
+        for(var a =0;a<winningCombinations.length;a++){
+          if(painted[winningCombinations[a][0]]==symbol && painted[winningCombinations[a][1]]==symbol && painted[winningCombinations[a][2]]==symbol){
+              alert("yea! "+symbol+" won");
+            }
+          }
       }
